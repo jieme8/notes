00d25043189c255cb5393d5bc459f3daa625396f
jieme8 <jieme8@126.com>
2018-04-25 11:34:30 +0800

    up

---
+++ javascript/vue/vue-cli-nuxt/server/api/users.js
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+
+const router = Router()
+
+// Mock Users
+const users = [
+  { name: 'Alexandre' },
+  { name: 'Pooya' },
+  { name: 'Sébastien' },
+  { name: 'aaaa' },
+]
+
+/* GET users listing. */
+router.get('/users', function (req, res, next) {
+  // res.send("12312312313")
+  res.json(users)
+})
+
+/* GET user by ID. */
+router.get('/users/:id', function (req, res, next) {
+  const id = parseInt(req.params.id)
+  if (id >= 0 && id < users.length) {
+    res.json(users[id])
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+export default router
