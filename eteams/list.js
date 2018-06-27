026a1b69c4e0fe79f50b50421b1a05833b1725de
jieme <jieme8@qq.com>
2018-06-27 11:09:23 +0800

    up

--- eteams/list.js
+++ eteams/list.js
@@ -20,11 +20,6 @@ let ac = require("./access_token");
                 console.log(`访问地址：${response.request.uri.href}`);
                 console.log(`状态吗：${response.statusCode}`);
                 console.log(body);
-                console.log(body.department[0].parent);
-                console.log(body.department[1].parent);
-                console.log(body.department[2].parent);
-                console.log(body.department[3].parent);
-                
             }
         });
     }catch(e){
