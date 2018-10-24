ab34316592ed5733ac63addd219c25b2e8ce311a
jieme <jieme8@qq.com>
2018-10-24 17:27:18 +0800

    up

--- eteams/xs/create.js
+++ eteams/xs/create.js
@@ -15,7 +15,7 @@ let ac = require("../access_token");
             "entity": JSON.stringify({
                 "name": "tt",
                 "manager": {
-                    "name": "胡杰"
+                    "username": "胡杰"
                 },
                 "clueSource": {
                     "name": "搜索引擎"
